7d7987b6d3f6a9cf122ca3400835be8842f58cbe
Jegathesan Shanmugam <nullbyte.in@gmail.com>
2023-04-11 11:57:40 +0530

    Integrate TimeMeasurement class for benchmarking Redis protobuf - some bugs are there

--- redis_protobuf/redis_publisher.cpp
+++ redis_protobuf/redis_publisher.cpp
@@ -1,35 +1,92 @@
 #include <iostream>
 #include <hiredis/hiredis.h>
 #include <opencv2/opencv.hpp>
+#include <chrono>
 #include "image.pb.h"
+#include "utils.hpp"
+
+#define MSG_SIZE_TEST 10000
+
+class RedisImagePublisher {
+public:
+    RedisImagePublisher(const std::string& host, int port)
+        : host_(host), port_(port), context_(nullptr) {
+        connect();
+    }
+
+    ~RedisImagePublisher() {
+        if (context_) {
+            redisFree(context_);
+        }
+    }
+
+    void connect() {
+        context_ = redisConnect(host_.c_str(), port_);
+        if (context_ == nullptr || context_->err) {
+            if (context_) {
+                std::cerr << "Error connecting to Redis: " << context_->errstr << std::endl;
+                redisFree(context_);
+            } else {
+                std::cerr << "Can't allocate Redis context" << std::endl;
+            }
+            throw std::runtime_error("Cannot connect to Redis");
+        }
+    }
+
+    void serialize_image_frame(std::string &serialized, const cv::Mat& frame) {
+        trodes::proto::Image img_data;
+        img_data.set_rows(frame.rows);
+        img_data.set_cols(frame.cols);
+        img_data.set_elt_type(frame.type());
+        img_data.set_elt_size((int)frame.elemSize());
+        size_t dataSize = frame.rows * frame.cols * frame.elemSize();
+        img_data.set_mat_data(frame.data, dataSize);
+        img_data.set_timestamp(std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count());
+        if (!img_data.SerializeToString(&serialized)) {
+            throw std::runtime_error("Error serializing message");
+        }
+    }
+
+    void publish_image(const cv::Mat& frame, int global_count) {
+        std::string serialized;
+        timeMeasurement_.measure_time<void>(std::bind(&RedisImagePublisher::serialize_image_frame, this, std::ref(serialized), std::ref(frame)));
+
+        std::cout << "Publish Image : " << frame.rows << " * " << frame.cols << ": " << global_count << std::endl;
+        redisReply *reply = (redisReply *)redisCommand(context_, "PUBLISH image_channel %b", serialized.data(), serialized.size());
+        if (reply == nullptr) {
+            std::cerr << "Error publishing message to Redis: " << context_->errstr << std::endl;
+            throw std::runtime_error("Cannot publish image to Redis");
+        }
+        freeReplyObject(reply);
+    }
+
+    double mean_serialization_time(int num_messages) {
+        return timeMeasurement_.mean_time(num_messages);
+    }
+
+private:
+    std::string host_;
+    int port_;
+    redisContext* context_;
+    TimeMeasurement timeMeasurement_;
+};
 
 int main() {
+
+    // Create a RedisImagePublisher instance and connect to the Redis server
+    RedisImagePublisher publisher("127.0.0.1", 6379);
+
     // Create example OpenCV matrices (images)
     cv::Mat frame_1 = cv::Mat::eye(480, 640, CV_8UC3);
     cv::Mat frame_2 = cv::Mat::eye(1280, 960, CV_8UC3);
     cv::Mat frame_3 = cv::Mat::eye(1800, 1200, CV_8UC3);
     cv::Mat frame_4 = cv::Mat::eye(2100, 1500, CV_8UC3);
 
-    // Connect to the Redis server
-    redisContext *c = redisConnect("127.0.0.1", 6379);
-
-    // Check if the connection to the Redis server is successful
-    if (c == NULL || c->err) {
-        if (c) {
-            std::cerr << "Error connecting to Redis: " << c->errstr << std::endl;
-            redisFree(c);
-        } else {
-            std::cerr << "Can't allocate Redis context" << std::endl;
-        }
-        return EXIT_FAILURE;
-    }
-
     int count{0}, global_count{0};
-    cv::Mat frame;
 
-    // Publish images in a loop
+    cv::Mat frame;
     while (true) {
-        // Select the next frame to be published
+        // Assign the appropriate frame depending on the value of count
         if (count == 0) {
             frame = frame_1;
             count += 1;
@@ -44,45 +101,23 @@ int main() {
             count = 0;
         }
 
-        // Create an Image protobuf message
-        trodes::proto::Image serializableMat;
-
-        // Set the trivial fields
-        serializableMat.set_rows(frame.rows);
-        serializableMat.set_cols(frame.cols);
-        serializableMat.set_elt_type(frame.type());
-        serializableMat.set_elt_size((int)frame.elemSize());
-
-        // Set the matrix's raw data
-        size_t dataSize = frame.rows * frame.cols * frame.elemSize();
-        serializableMat.set_mat_data(frame.data, dataSize);
-
-        // Serialize the message using Protocol Buffers
-        std::string serialized;
-        if (!serializableMat.SerializeToString(&serialized)) {
-            std::cerr << "Error serializing message" << std::endl;
+        try {
+            publisher.publish_image(frame, global_count);
+        } catch (std::runtime_error& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
             break;
         }
 
-        global_count += 1;
-        std::cout << "Publish Image : " << frame.rows << " * " << frame.cols << ": " <<  global_count << std::endl;
-
-        // Publish the message to the Redis channel
-        redisReply *reply = (redisReply *)redisCommand(c, "PUBLISH image_channel %b", serialized.data(), serialized.size());
-        if (reply == NULL) {
-            std::cerr << "Error publishing message to Redis: " << c->errstr << std::endl;
-            break;
-        }
+        global_count++;
 
         // Terminate the loop after 2000 iterations
-        if (global_count == 2000) {
+        if (global_count == MSG_SIZE_TEST) {
+            double ser_time = publisher.mean_serialization_time(MSG_SIZE_TEST - 1);
+            std::cout << "Serialization Mean time: " << NS_TO_MS(ser_time) << " ms" << std::endl;
             exit(1);
         }
-
-        freeReplyObject(reply);
     }
 
-    // Free the Redis connection and exit successfully
-    redisFree(c);
     return EXIT_SUCCESS;
 }
+
--- redis_protobuf/redis_subscriber.cpp
+++ redis_protobuf/redis_subscriber.cpp
@@ -1,65 +1,133 @@
 #include <iostream>
 #include <hiredis/hiredis.h>
+#include <opencv2/core.hpp>
+#include <opencv2/dnn.hpp>
+#include <opencv2/dnn/all_layers.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
+
+#include "utils.hpp"
 #include "image.pb.h"
 
-int main() {
-    int global_count{0};
-    
-    // Connect to the Redis server
-    redisContext *c = redisConnect("127.0.0.1", 6379);
+#define MSG_SIZE_TEST 9000
 
-    // Check if the connection to the Redis server is successful
-    if (c == NULL || c->err) {
-        if (c) {
-            std::cerr << "Error connecting to Redis: " << c->errstr << std::endl;
-            redisFree(c);
-        } else {
-            std::cerr << "Can't allocate Redis context" << std::endl;
-        }
-        return EXIT_FAILURE;
+std::chrono::high_resolution_clock::time_point chrono_time;
+
+class RedisImageSubscriber {
+public:
+    RedisImageSubscriber(const std::string &ip_address, int port)
+        : ip_address_(ip_address), port_(port) {
+        connect_to_redis();
     }
 
-    // Subscribe to the Redis channel
-    redisReply *reply = (redisReply *)redisCommand(c, "SUBSCRIBE image_channel");
-    freeReplyObject(reply);
-
-    // Listen for incoming messages
-    while (redisGetReply(c, (void **)&reply) == REDIS_OK) {
-        // Check for errors in receiving messages
-        if (reply == NULL || reply->type == REDIS_REPLY_ERROR) {
-            std::cerr << "Error receiving message from Redis: " << c->errstr << std::endl;
-            break;
+    ~RedisImageSubscriber() {
+        if (redis_context_) {
+            redisFree(redis_context_);
         }
+    }
 
-        // Check if the reply contains a message
-        if (reply->type == REDIS_REPLY_ARRAY && reply->elements == 3 &&
-            std::string(reply->element[0]->str) == "message") {
-            // Deserialize the message using protobuf
-            trodes::proto::Image serializableMat;
-            serializableMat.ParseFromArray(reply->element[2]->str, reply->element[2]->len);
+    void connect_to_redis() {
+        redis_context_ = redisConnect(ip_address_.c_str(), port_);
 
-            try {
-                // Convert the serialized matrix to an OpenCV Mat object
-                cv::Mat img(serializableMat.rows(), serializableMat.cols(), serializableMat.elt_type());
-                size_t dataSize = serializableMat.rows() * serializableMat.cols() * serializableMat.elt_size();
-                memcpy(img.data, serializableMat.mat_data().data(), dataSize);
-                global_count += 1;
-                std::cout << "Received image: " << img.rows << " * " << img.cols << ": " << global_count << std::endl;
-
-                // Display the image (uncomment the lines below to display the image)
-                // cv::imshow("Image", img);
-                // cv::waitKey(1);
-            } catch (const std::exception &e) {
-                std::cerr << "Error deserializing message: " << e.what() << std::endl;
+        if (redis_context_ == NULL || redis_context_->err) {
+            if (redis_context_) {
+                std::cerr << "Error connecting to Redis: " << redis_context_->errstr << std::endl;
+                redisFree(redis_context_);
+            } else {
+                std::cerr << "Can't allocate Redis context" << std::endl;
             }
+            throw std::runtime_error("Failed to connect to Redis server");
         }
+    }
 
-        // Free the reply object
+    void subscribe(const std::string &channel_name) {
+        redisReply *reply = (redisReply *)redisCommand(redis_context_, "SUBSCRIBE %s", channel_name.c_str());
+        if (reply == NULL || reply->type == REDIS_REPLY_ERROR) {
+            std::cerr << "Error subscribing to channel: " << redis_context_->errstr << std::endl;
+            throw std::runtime_error("Failed to subscribe to Redis channel");
+        }
         freeReplyObject(reply);
     }
 
-    // Free the Redis connection and exit successfully
-    redisFree(c);
+    redisReply *get_message() {
+        redisReply *reply;
+        if (redisGetReply(redis_context_, (void **)&reply) == REDIS_OK) {
+            return reply;
+        } else {
+            return nullptr;
+        }
+    }
+
+    cv::Mat deserialize_image(redisReply* reply) {
+        trodes::proto::Image serializableMat;
+        serializableMat.ParseFromArray(reply->element[2]->str, reply->element[2]->len);
+        chrono_time = std::chrono::system_clock::time_point(std::chrono::nanoseconds(serializableMat.timestamp()));
+
+        cv::Mat img;
+
+        try {
+            img = cv::Mat(serializableMat.rows(), serializableMat.cols(), serializableMat.elt_type());
+            size_t dataSize = serializableMat.rows() * serializableMat.cols() * serializableMat.elt_size();
+            memcpy(img.data, serializableMat.mat_data().data(), dataSize);
+            
+        } catch (const std::exception &e) {
+            std::cerr << "Error deserializing message: " << e.what() << std::endl;
+        }
+        return img;
+    }
+
+private:
+    std::string ip_address_;
+    int port_;
+    redisContext *redis_context_{nullptr};
+};
+
+int main() {
+    TimeMeasurement timeMeasurement_;
+    std::atomic<bool> exit_flag_{false};
+    size_t global_count{0};
+    std::chrono::nanoseconds::rep time_diff_ns_{0};
+
+    // Create a RedisImageSubscriber instance and connect to the Redis server
+    RedisImageSubscriber subscriber("127.0.0.1", 6379);
+    subscriber.subscribe("image_channel");
+
+    redisReply *reply;
+    while ((reply = subscriber.get_message()) != nullptr) {
+        if (reply->type == REDIS_REPLY_ERROR) {
+            std::cerr << "Error receiving message from Redis: " << reply->str << std::endl;
+            break;
+        }
+
+        if (exit_flag_) {
+            double latency = static_cast<double>(time_diff_ns_) / global_count;
+            std::cout << "Latency: " << NS_TO_MS(latency) << " ms" << std::endl;
+            double deser_time = timeMeasurement_.mean_time(MSG_SIZE_TEST);
+            std::cout << "Deserialization Mean time: " << NS_TO_MS(deser_time) << " ms" << std::endl;
+            break;
+        }
+
+        auto current_chrono_time = std::chrono::high_resolution_clock::now();
+
+        if (reply->type == REDIS_REPLY_ARRAY && reply->elements == 3 &&
+            std::string(reply->element[0]->str) == "message") {
+            auto time_diff = current_chrono_time - chrono_time;
+            
+            cv::Mat frame = timeMeasurement_.measure_time<cv::Mat>(std::bind(&RedisImageSubscriber::deserialize_image, &subscriber, std::ref(reply)));
+
+            time_diff_ns_ += std::chrono::duration_cast<std::chrono::nanoseconds>(time_diff).count();
+            std::cout << "time_diff_ns_" << time_diff_ns_ << std::endl;
+
+            std::cout << "Received image: " << frame.rows << " * " << frame.cols << ": " << global_count++ << std::endl;
+
+            if (global_count == MSG_SIZE_TEST) {
+                exit_flag_ = true;
+            }
+        }
+
+        freeReplyObject(reply);
+    }
+
     return EXIT_SUCCESS;
 }
