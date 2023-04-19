34207e48a697c7f1ed3a29c567e594a053095b46
Jegathesan Shanmugam <nullbyte.in@gmail.com>
2023-04-19 18:48:14 +0530

    Integrate TimeMeasurement class for benchmarking gRPC

--- zmq_msgpack/zmq_publisher.cpp
+++ zmq_msgpack/zmq_publisher.cpp
@@ -1,63 +1,116 @@
-#include "image.hpp"
-#include <chrono>   
+#include <chrono>
 #include <opencv2/opencv.hpp>
+#include <chrono>
+#include <ctime>
+#include <iostream>
+#include <msgpack.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/dnn.hpp>
+#include <opencv2/dnn/all_layers.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <thread>
+#include <zmq.hpp>
+#include <zmq_addon.hpp>
 
-int main()
-{
-    zmq::context_t ctx{1};
-    zmq::socket_t server{ctx, zmq::socket_type::rep};
-    server.bind("tcp://127.0.0.1:5555");
+#include "utils.hpp"
 
-    // Create example OpenCV matrices (images)
-    cv::Mat frame_1 = cv::Mat::eye(480, 640, CV_8UC3);
-    cv::Mat frame_2 = cv::Mat::eye(1280, 960, CV_8UC3);
-    cv::Mat frame_3 = cv::Mat::eye(1800, 1200, CV_8UC3);
-    cv::Mat frame_4 = cv::Mat::eye(2100, 1500, CV_8UC3);
-    
-    int count{0}, global_count{0};
-    cv::Mat frame;
-    while (true) {
-        // Assign the appropriate frame depending on the value of count
-        if (count == 0) {
-            frame = frame_1;
-            count += 1;
-        } else if (count == 1) {
-            frame = frame_2;
-            count += 1;
-        } else if (count == 2) {
-            frame = frame_3;
-            count += 1;
-        } else {
-            frame = frame_4;
-            count = 0;
+#define MSG_SIZE_TEST 10000
+
+struct Image {
+    std::vector<uchar> matrix;
+    int rows{0};
+    int cols{0};
+    int type{0};
+    std::int64_t timestamp_ns{0};
+    MSGPACK_DEFINE(matrix, rows, cols, type, timestamp_ns);
+};
+
+class ImagePublisher {
+public:
+    ImagePublisher(const std::string& address)
+        : ctx_(1), server_(ctx_, zmq::socket_type::rep) {
+        server_.bind(address);
+    }
+
+    void run() {
+        int count{0}, global_count{0};
+        cv::Mat frame;
+        while (true) {
+            // Assign the appropriate frame depending on the value of count
+            if (count == 0) {
+                frame = frame_1_;
+                count += 1;
+            } else if (count == 1) {
+                frame = frame_2_;
+                count += 1;
+            } else if (count == 2) {
+                frame = frame_3_;
+                count += 1;
+            } else {
+                frame = frame_4_;
+                count = 0;
+            }
+
+            // Open for requests
+            std::cout << "open for request..." << std::endl;
+            zmq::message_t req;
+            server_.recv(req, zmq::recv_flags::none);
+            std::cout << "[⚡ got request] => " << req.to_string() << std::endl;
+
+            zmq::message_t packed_msg = timeMeasurement_.measure_time<zmq::message_t>(
+                std::bind(&ImagePublisher::serialize_image_frame, this, std::ref(frame)));
+
+            std::cout << "[⚓ Publish image: " << frame.rows << " * " << frame.cols << ": " << global_count++ << "] =>" << std::endl;
+
+            server_.send(packed_msg, zmq::send_flags::none);
+
+            if (global_count == MSG_SIZE_TEST) {
+                double ser_time = timeMeasurement_.mean_time(MSG_SIZE_TEST - 1);
+                std::cout << "Serialization Mean time: " << NS_TO_MS(ser_time) << " ms" << std::endl;
+                exit(1);
+            }
         }
-        // open for requests 
-        std::cout << "open for request..." << std::endl;
-        zmq::message_t req;
-        server.recv(req, zmq::recv_flags::none);
-        std::cout << "[⚡ got request] => " << req.to_string() << std::endl;
+    }
 
-        // prepare data
+private:
+    zmq::message_t serialize_image_frame(const cv::Mat& frame) {
+        // Prepare data
         Image img_data;
+
         img_data.matrix = std::vector<uchar>(frame.data, frame.data + (frame.rows * frame.cols * frame.channels()));
         img_data.rows = frame.rows;
         img_data.cols = frame.cols;
         img_data.type = frame.type();
+        auto timestamp = std::chrono::high_resolution_clock::now();
+        img_data.timestamp_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(timestamp.time_since_epoch()).count();
 
-        // packed/serialize the data using msgpack
+        // Packed/serialize the data using msgpack
         msgpack::sbuffer serialized_img;
         msgpack::pack(&serialized_img, img_data);
-        std::cout << "[⚓ Publish image: " << frame.rows << " * " << frame.cols << ": " << global_count++ << "] =>"<< std::endl;
-        // send the serialized data as the response to client
+
+        // Send the serialized data as the response to client
         zmq::message_t packed_msg(serialized_img.size());
         std::memcpy(packed_msg.data(), serialized_img.data(), serialized_img.size());
-        server.send(packed_msg, zmq::send_flags::none);
-        
-        // Terminate the loop after 2000 iterations
-        if (global_count == 2000) {
-            exit(1);
-        }
+
+        return packed_msg;
     }
+
+    zmq::context_t ctx_;
+    zmq::socket_t server_;
+    TimeMeasurement timeMeasurement_;
+
+    cv::Mat frame_1_ = cv::Mat::eye(480, 640, CV_8UC3);
+    cv::Mat frame_2_ = cv::Mat::eye(1280, 960, CV_8UC3);
+    cv::Mat frame_3_ = cv::Mat::eye(1800, 1200, CV_8UC3);
+    cv::Mat frame_4_ = cv::Mat::eye(2100, 1500, CV_8UC3);
+};
+
+int main() {
+    ImagePublisher publisher("tcp://127.0.0.1:5555");
+    publisher.run();
 
     return EXIT_SUCCESS;
 }
--- zmq_msgpack/zmq_subscriber.cpp
+++ zmq_msgpack/zmq_subscriber.cpp
@@ -1,44 +1,96 @@
+#include <chrono>
+#include <iostream>
+#include <msgpack.hpp>
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <zmq.hpp>
+#include <zmq_addon.hpp>
 
-#include "image.hpp"
+#include "utils.hpp"
 
-int main()
-{
-  int global_count{0};
-  zmq::context_t ctx{1};
-  zmq::socket_t client{ctx, zmq::socket_type::req};
-  client.connect("tcp://127.0.0.1:5555");
+#define MSG_SIZE_TEST 10000
 
-  const std::string req_data{"asking for cv::Mat"};
+struct Image {
+    std::vector<uchar> matrix;
+    int rows{0};
+    int cols{0};
+    int type{0};
+    std::int64_t timestamp_ns{0};
+    MSGPACK_DEFINE(matrix, rows, cols, type, timestamp_ns);
+};
 
-  while (true) {
-    // Send a request
-    std::cout << "sending request... " << std::endl;
-    client.send(zmq::buffer(req_data), zmq::send_flags::none);
+class ImageSubscriber {
+public:
+    ImageSubscriber(const std::string& server_address)
+        : ctx_(1), client_(ctx_, zmq::socket_type::req) {
+        client_.connect(server_address);
+    }
 
-    // Get the response data
-    zmq::message_t response_data;
-    client.recv(response_data, zmq::recv_flags::none);
-    
-    // Deserialize the response data
-    msgpack::unpacked unpacked_response_data;
-    msgpack::unpack(unpacked_response_data,
-                    static_cast<const char*>(response_data.data()),
-                    response_data.size());
+    cv::Mat deserialize_image(zmq::message_t& response_data) {
+        // Deserialize the response data
+        msgpack::unpacked unpacked_response_data;
+        msgpack::unpack(unpacked_response_data,
+                        static_cast<const char*>(response_data.data()),
+                        response_data.size());
 
-    auto unpacked_response = unpacked_response_data.get().as<Image>();
+        auto unpacked_response = unpacked_response_data.get().as<Image>();
+        chrono_time_ = std::chrono::high_resolution_clock::time_point(
+            std::chrono::nanoseconds(unpacked_response.timestamp_ns));
 
-    cv::Mat img =  cv::Mat(unpacked_response.rows, unpacked_response.cols,
-                unpacked_response.type, unpacked_response.matrix.data());
+        return cv::Mat(unpacked_response.rows, unpacked_response.cols,
+                       unpacked_response.type, unpacked_response.matrix.data());
+    }
 
-    std::cout << "[⚓ Received image: " << unpacked_response.rows << " * " << unpacked_response.cols << ": " << global_count++ << "] =>"<< std::endl;
+    void run() {
+        TimeMeasurement timeMeasurement_;
+        int global_count{0};
+        std::chrono::nanoseconds::rep time_diff_ns_{0};
+        const std::string req_data{"asking for cv::Mat"};
 
-    // Return back the vector<uchar> to cv::Mat image and show it 
-    // cv::imshow(
-    //     "Result",
-    //     cv::Mat(unpacked_response.rows, unpacked_response.cols,
-    //             unpacked_response.type, unpacked_response.matrix.data()));
-    // cv::waitKey(1);                
-  }
-  
-  return EXIT_SUCCESS;
+        while (true) {
+            // Send a request
+            std::cout << "sending request... " << std::endl;
+            client_.send(zmq::buffer(req_data), zmq::send_flags::none);
+
+            // Get the response data
+            zmq::message_t response_data;
+            client_.recv(response_data, zmq::recv_flags::none);
+            auto current_chrono_time =
+                std::chrono::high_resolution_clock::now();
+            cv::Mat frame = timeMeasurement_.measure_time<cv::Mat>(
+                std::bind(&ImageSubscriber::deserialize_image, this,
+                          std::ref(response_data)));
+
+            auto time_diff = current_chrono_time - chrono_time_;
+            time_diff_ns_ +=
+                std::chrono::duration_cast<std::chrono::nanoseconds>(time_diff)
+                    .count();
+
+            std::cout << "[⚓ Received image: " << frame.rows << " * "
+                      << frame.cols << ": " << global_count++ << "] =>"
+                      << std::endl;
+
+            if (global_count == MSG_SIZE_TEST) {
+                double latency =
+                    static_cast<double>(time_diff_ns_) / global_count;
+                std::cout << "Latency: " << NS_TO_MS(latency) << " ms"
+                          << std::endl;
+                double deser_time = timeMeasurement_.mean_time(MSG_SIZE_TEST);
+                std::cout << "Deserialization Mean time: "
+                          << NS_TO_MS(deser_time) << " ms" << std::endl;
+            }
+        }
+    }
+
+private:
+    zmq::context_t ctx_;
+    zmq::socket_t client_;
+    std::chrono::high_resolution_clock::time_point chrono_time_;
+};
+
+int main() {
+    std::string server_address = "tcp://127.0.0.1:5555";
+    ImageSubscriber subscriber(server_address);
+    subscriber.run();
+    return EXIT_SUCCESS;
 }
